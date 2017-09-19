4f21bc14a9da3cc64510853a377ac79ee35ebd7f
Nicholas-Kyle <nickdotkyle@hotmail.com>
2017-09-19 14:54:10 +0100

    Add files via upload

--- androidstudioprojects/myapplication/app/src/main/java/com/bangtidygames/birdstrike/MainMenuScreen.java
+++ androidstudioprojects/myapplication/app/src/main/java/com/bangtidygames/birdstrike/MainMenuScreen.java
@@ -18,6 +18,7 @@ public class MainMenuScreen extends Screen {
     private boolean dragging = false;
 
     private int x = 0;
+    private int maxMenuX = 480;
     private int currentDrag;
     private int drag;
 
@@ -46,11 +47,6 @@ public class MainMenuScreen extends Screen {
 
             if (event.type == TouchEvent.TOUCH_UP) {
                 if (dragging == false) {
-
-                    //  Enter level screen positions here
-                    // add system to adjust screen positions for future updates
-                    // and to share screen positions with graphics for buttons
-
                     for (MenuLevelPosition level : LevelPositions) {
                         if (inBounds(event, level.getX() - x, level.getY(), 119, 150)) {
                             if (LoadSave.getHearts() != 0) {
@@ -65,6 +61,9 @@ public class MainMenuScreen extends Screen {
                     currentDrag = 0;
                     if (x < 0){
                         x = 0;
+                    }
+                    if (x > maxMenuX){
+                        x = maxMenuX;
                     }
                     dragging = false;
                 }
@@ -85,10 +84,25 @@ public class MainMenuScreen extends Screen {
     public void paint(float deltaTime) {
         Graphics g = game.getGraphics();
         int menuPos = x+currentDrag;
-        if (menuPos > 0) {
+        if (menuPos > 0 && menuPos < maxMenuX) {
             g.drawImage(Assets.menu_forest, (0-menuPos), 0);
+            g.drawImage(Assets.menu_forest, (1280-menuPos), 0);
+            g.drawImage(Assets.menu_forest, (2560-menuPos), 0);
+            g.drawImage(Assets.menu_forest, (3840-menuPos), 0);
             for (MenuLevelPosition level : LevelPositions) {
-                g.drawImage(Assets.level_1, level.getX() - menuPos, level.getY());
+                if(level.getLevelNumber() == 1) {
+                    g.drawImage(Assets.level_1, level.getX() - menuPos, level.getY());
+                } else if(level.getLevelNumber() == 2) {
+                    g.drawImage(Assets.level_2, level.getX() - menuPos, level.getY());
+                } else if(level.getLevelNumber() == 3) {
+                    g.drawImage(Assets.level_3, level.getX() - menuPos, level.getY());
+                } else if(level.getLevelNumber() == 4) {
+                    g.drawImage(Assets.level_4, level.getX() - menuPos, level.getY());
+                } else if(level.getLevelNumber() == 5) {
+                    g.drawImage(Assets.level_5, level.getX() - menuPos, level.getY());
+                } else if(level.getLevelNumber() == 6) {
+                    g.drawImage(Assets.level_6, level.getX() - menuPos, level.getY());
+                }
                 if (LoadSave.stars[level.getLevelNumber()-1] == 1){
                     g.drawImage(Assets.one_star, level.getX() - menuPos, level.getY());
                 } else if (LoadSave.stars[level.getLevelNumber()-1] == 2) {
@@ -97,10 +111,22 @@ public class MainMenuScreen extends Screen {
                     g.drawImage(Assets.three_stars, level.getX() - menuPos, level.getY());
                 }
             }
-        } else {
+        } else if (menuPos <= 0){
             g.drawImage(Assets.menu_forest, (0), 0);
             for (MenuLevelPosition level : LevelPositions) {
-                g.drawImage(Assets.level_1, level.getX(), level.getY());
+                if(level.getLevelNumber() == 1) {
+                    g.drawImage(Assets.level_1, level.getX(), level.getY());
+                } else if(level.getLevelNumber() == 2) {
+                    g.drawImage(Assets.level_2, level.getX(), level.getY());
+                } else if(level.getLevelNumber() == 3) {
+                    g.drawImage(Assets.level_3, level.getX(), level.getY());
+                } else if(level.getLevelNumber() == 4) {
+                    g.drawImage(Assets.level_4, level.getX(), level.getY());
+                } else if(level.getLevelNumber() == 5) {
+                    g.drawImage(Assets.level_5, level.getX(), level.getY());
+                } else if(level.getLevelNumber() == 6) {
+                    g.drawImage(Assets.level_6, level.getX(), level.getY());
+                }
                 if (LoadSave.stars[level.getLevelNumber()-1] == 1){
                     g.drawImage(Assets.one_star, level.getX(), level.getY());
                 } else if (LoadSave.stars[level.getLevelNumber()-1] == 2) {
@@ -109,11 +135,34 @@ public class MainMenuScreen extends Screen {
                     g.drawImage(Assets.three_stars, level.getX(), level.getY());
                 }
             }
+        } else if (menuPos > maxMenuX){
+            g.drawImage(Assets.menu_forest, (0-maxMenuX), 0);
+            g.drawImage(Assets.menu_forest, (1280-maxMenuX), 0);
+            g.drawImage(Assets.menu_forest, (2560-maxMenuX), 0);
+            g.drawImage(Assets.menu_forest, (3840-maxMenuX), 0);
+            for (MenuLevelPosition level : LevelPositions) {
+                if(level.getLevelNumber() == 1) {
+                    g.drawImage(Assets.level_1, level.getX()-maxMenuX, level.getY());
+                } else if(level.getLevelNumber() == 2) {
+                    g.drawImage(Assets.level_2, level.getX()-maxMenuX, level.getY());
+                } else if(level.getLevelNumber() == 3) {
+                    g.drawImage(Assets.level_3, level.getX()-maxMenuX, level.getY());
+                } else if(level.getLevelNumber() == 4) {
+                    g.drawImage(Assets.level_4, level.getX()-maxMenuX, level.getY());
+                } else if(level.getLevelNumber() == 5) {
+                    g.drawImage(Assets.level_5, level.getX()-maxMenuX, level.getY());
+                } else if(level.getLevelNumber() == 6) {
+                    g.drawImage(Assets.level_6, level.getX()-maxMenuX, level.getY());
+                }
+                if (LoadSave.stars[level.getLevelNumber()-1] == 1){
+                    g.drawImage(Assets.one_star, level.getX()-maxMenuX, level.getY());
+                } else if (LoadSave.stars[level.getLevelNumber()-1] == 2) {
+                    g.drawImage(Assets.two_stars, level.getX()-maxMenuX, level.getY());
+                } else if (LoadSave.stars[level.getLevelNumber()-1] == 3) {
+                    g.drawImage(Assets.three_stars, level.getX()-maxMenuX, level.getY());
+                }
+            }
         }
-        g.drawImage(Assets.menu_forest, (1280-menuPos), 0);
-        g.drawImage(Assets.menu_forest, (2560-menuPos), 0);
-        g.drawImage(Assets.menu_forest, (3840-menuPos), 0);
-
     }
 
     private void selectLevel(int level) {
@@ -129,6 +178,14 @@ public class MainMenuScreen extends Screen {
         LevelPositions.add(level1);
         MenuLevelPosition level2 = new MenuLevelPosition((x+gapX*1), (y+gapY*1), 2);
         LevelPositions.add(level2);
+        MenuLevelPosition level3 = new MenuLevelPosition((x+gapX*2), y, 3);
+        LevelPositions.add(level3);
+        MenuLevelPosition level4 = new MenuLevelPosition((x+gapX*3), (y+gapY*1), 4);
+        LevelPositions.add(level4);
+        MenuLevelPosition level5 = new MenuLevelPosition((x+gapX*4), y, 5);
+        LevelPositions.add(level5);
+        MenuLevelPosition level6 = new MenuLevelPosition((x+gapX*5), (y+gapY*1), 6);
+        LevelPositions.add(level6);
     }
 
     @Override
